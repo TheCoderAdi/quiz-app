11fd3dded5d4bf0cf01a672dcab8e55bd0e6f047
Aditya <adityasahoo652@gmail.com>
2023-11-18 22:56:25 +0530

    Remove unnecessary styles and update question options

--- constants/data.js
+++ constants/data.js
@@ -21,38 +21,38 @@ export const questions = [
     ],
   },
   {
-    question: "What is 2 + 2?",
+    question: "What is 2 + 3?",
     options: [
       { text: "3", isCorrect: false },
-      { text: "4", isCorrect: true },
+      { text: "4", isCorrect: false },
+      { text: "5", isCorrect: true },
+      { text: "6", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is 2 + 4?",
+    options: [
+      { text: "3", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "5", isCorrect: false },
+      { text: "6", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is 1 + 2?",
+    options: [
+      { text: "3", isCorrect: true },
+      { text: "4", isCorrect: false },
       { text: "5", isCorrect: false },
       { text: "6", isCorrect: false },
     ],
   },
   {
-    question: "What is 2 + 2?",
+    question: "What is 3 + 2?",
     options: [
       { text: "3", isCorrect: false },
-      { text: "4", isCorrect: true },
-      { text: "5", isCorrect: false },
-      { text: "6", isCorrect: false },
-    ],
-  },
-  {
-    question: "What is 2 + 2?",
-    options: [
-      { text: "3", isCorrect: false },
-      { text: "4", isCorrect: true },
-      { text: "5", isCorrect: false },
-      { text: "6", isCorrect: false },
-    ],
-  },
-  {
-    question: "What is 2 + 2?",
-    options: [
-      { text: "3", isCorrect: false },
-      { text: "4", isCorrect: true },
-      { text: "5", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "5", isCorrect: true },
       { text: "6", isCorrect: false },
     ],
   },
